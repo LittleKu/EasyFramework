a729c24fda52607b5a62ad116003a2134be96faf
LittleKu <lvzhaolang@cmcm.com>
2023-12-07 09:14:29 +0800

    Add weak ref interface

--- easy_framework/include/ef_base.h
+++ easy_framework/include/ef_base.h
@@ -1,6 +1,8 @@
 #ifndef EASY_FRAMEWORK_INCLUDE_BASE_H_
 #define EASY_FRAMEWORK_INCLUDE_BASE_H__
 
+struct IWeakObject;
+
 struct IBaseInterface {
   /**
    * @brief
@@ -24,5 +26,44 @@ struct IBaseInterface {
    */
   virtual bool QueryInterface(const char* interface_unique,
                               IBaseInterface** out_interface) const = 0;
-}
+
+  /**
+   * @brief Get the Weak Ref object
+   *
+   * @param out_weak_ref
+   * @return true on success,false on failure
+   */
+  virtual bool GetWeakRef(IWeakObject** out_weak_ref) const = 0;
+};
+
+struct IWeakObject {
+  /**
+   * @brief
+   *
+   */
+  virtual void AddRef() const = 0;
+
+  /**
+   * @brief
+   *
+   * @return true will release object,otherwise decrement reference
+   */
+  virtual bool Release() const = 0;
+
+  /**
+   * @brief Deteting the validity of the host pointer.
+   *
+   * @return true on invalid, otherwise valid.
+   */
+  virtual bool Expired() const = 0;
+
+  /**
+   * @brief Get the host pointer as a strong reference
+   *
+   * @param strong_ptr
+   * @return true success, otherwise failure
+   */
+  virtual bool Lock(IBaseInterface** strong_ptr) = 0;
+};
+
 #endif  // !EASY_FRAMEWORK_INCLUDE_BASE_H__
