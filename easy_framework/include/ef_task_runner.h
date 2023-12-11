d9b69ed6d282149002ec737afbf57b57ed6a6b59
LittleKu <lvzhaolang@cmcm.com>
2023-12-11 20:39:19 +0800

    add task runner interface

---
+++ easy_framework/include/ef_task_runner.h
@@ -0,0 +1,31 @@
+#ifndef EASY_FRAMEWORK_TASK_RUNNER_H_
+#define EASY_FRAMEWORK_TASK_RUNNER_H_
+
+#include "easy_framework/include/ef_base.h"
+
+struct ITask : public IRefBase {
+  /**
+   * @brief Run task
+   *
+   */
+  virtual void Run() = 0;
+};
+
+struct ITaskRunner : public IBaseInterface {
+  /**
+   * @brief Post task
+   *
+   * @param task
+   */
+  virtual void PostTask(ITask* task) = 0;
+
+  /**
+   * @brief Post delayed task
+   *
+   * @param task
+   * @param ms milliseconds
+   */
+  virtual void PostDelayedTask(ITask* task, long long ms) = 0;
+};
+
+#endif  // !EASY_FRAMEWORK_TASK_RUNNER_H_
