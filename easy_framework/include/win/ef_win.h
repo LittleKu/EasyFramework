04329d9fa091102492a92b2deee7a602c6928c0a
LittleKu <kklvzl@gmail.com>
2023-12-27 21:38:33 +0800

    add plugin CanUnloadPlugin export function

--- easy_framework/include/win/ef_win.h
+++ easy_framework/include/win/ef_win.h
@@ -6,11 +6,13 @@
 struct IBaseInterface;
 
 typedef HRESULT(__stdcall* FN_QUERY_INTERFACE)(const char*, IBaseInterface**);
+typedef HRESULT(__stdcall* FN_CAN_UNLOAD_PLUGIN)();
 
 typedef HMODULE PlatformLibrary;
 
 typedef DWORD ThreadId;
 
 STDAPI QueryInterface(const char*, IBaseInterface**);
+STDAPI CanUnloadPlugin();
 
 #endif  // EASY_FRAMEWORK_INCLUDE_WIN_H_
