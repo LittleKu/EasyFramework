1d3a405fea660e4578a9bcb2468e9b960165c914
LittleKu <kklvzl@gmail.com>
2025-04-15 17:18:42 +0800

    更新libef工程;更新example工程

---
+++ eframework/src/eframework.cc
@@ -0,0 +1,26 @@
+﻿/**
+ * @file      : eframework.cc
+ * @author    : LittleKu<kklvzl@gmail.com>
+ * @date      : 2025-04-15 16:52:29
+ * @brief     : 
+ */
+#include "libef/eframework/src/eframework.h"
+
+#include "build/build_config.h"
+#include "libef/include/exports.h"
+
+
+#if BUILDFLAG(IS_WIN)
+#include "base/win/windows_full.h"
+
+BOOL APIENTRY DllMain(HMODULE module,
+                      DWORD reason,
+                      LPVOID reserved) {
+  if (reason== DLL_PROCESS_ATTACH) {
+    DisableThreadLibraryCalls(module);
+  }
+  return TRUE;
+}
+#elif BUILDFLAG(IS_LINUX)
+
+#endif
