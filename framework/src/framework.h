b21e7cc98813ca8a46910bc681d7c37aeddb3c9c
LittleKu <kklvzl@gmail.com>
2025-04-15 21:26:43 +0800

    增加Framework接口

---
+++ framework/src/framework.h
@@ -0,0 +1,50 @@
+﻿/**
+ * @file      : framework.h
+ * @author    : LittleKu<kklvzl@gmail.com>
+ * @date      : 2025-04-15 20:06:10
+ * @brief     : 
+ */
+#ifndef __FRAMEWORK_H__
+#define __FRAMEWORK_H__
+
+#include "build/build_config.h"
+
+#include "libef/wrapper/ref_impl.h"
+
+#if BUILDFLAG(IS_WIN)
+#include "base/win/windows_types.h"
+using Instance = HINSTANCE;
+#elif BUILDFLAG(IS_LINUX)
+//TODO add linux header ?
+using Instance = void*;
+#endif
+
+#include <atomic>
+
+namespace libef {
+
+class FrameworkImpl : public BaseRefImpl<Framework> {
+ public:
+  FrameworkImpl();
+  ~FrameworkImpl() override;
+
+ public: /* override BaseInterface */
+  bool QueryInterface(const char* interface_name, void** interface) override;
+  unsigned int GetVersion() override;
+
+ public:
+  bool Initialize(void* instance) override;
+  void UnInitialize() override;
+
+ private:
+  std::atomic_bool initialized_{false};
+#if BUILDFLAG(IS_WIN)
+  Instance instance_{nullptr};
+#elif BUILDFLAG(IS_LINUX)
+
+#endif
+};
+
+}  // namespace libef
+
+#endif /* __FRAMEWORK_H__ */
--- include/libef.h
+++ include/libef.h
@@ -8,7 +8,6 @@
 #define __LIBEF_H__
 
 #if defined(_WIN32)
-
 #if defined(FRAMEWORK_IMPLEMENTATION)
 #define FRAMEWORK_EXPORT __declspec(dllexport)
 #else
@@ -16,26 +15,49 @@
 #endif
 
 #elif defined(__linux__)
-
 #if defined(FRAMEWORK_IMPLEMENTATION)
 #define FRAMEWORK_EXPORT __attribute__((visibility("default")))
 #else
 #define FRAMEWORK_EXPORT
 #endif
-
 #else
 #define FRAMEWORK_EXPORT
 #endif
+extern "C" FRAMEWORK_EXPORT bool QueryInterface(
+    /*[in]*/ const char* interface_name,
+    /*[out]*/ void** interface);
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
+class BaseRef {
+ public:
+  virtual ~BaseRef() {}
+  virtual void AddRef() const = 0;
+  virtual bool Release() const = 0;
+};
 
-FRAMEWORK_EXPORT bool QueryInterface(/*[in]*/ const char* interface_name,
-                                     /*[out]*/ void** interface);
+class BaseInterface : public BaseRef {
+ public:
+  /**
+   * \brief 通过接口名称获取指定接口实例对象
+   * 
+   * \param interface_name 接口名称
+   * \param interface 接口实例对象
+   * \return true 获取成功,false 获取失败
+   */
+  virtual bool QueryInterface(const char* interface_name, void** interface) = 0;
+  /**
+   * \brief 获取对应接口版本号.
+   * 
+   * \return 接口版本号
+   */
+  virtual unsigned int GetVersion() = 0;
+};
 
-#if defined(__cplusplus)
-}
-#endif
+#define FRAMEWORK_NAME "IFrameworkV1"
+#define FRAMEWORK_VERSION 1
+class Framework : public BaseInterface {
+ public:
+  virtual bool Initialize(void* instance) = 0;
+  virtual void UnInitialize() = 0;
+};
 
 #endif /* __LIBEF_H__ */
