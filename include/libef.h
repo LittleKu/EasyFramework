425f1c344d31e0a95d8cd1585cb33f1d5c79a4c1
LittleKu <kklvzl@gmail.com>
2025-05-07 19:35:15 +0800

    增加注释

--- include/libef.h
+++ include/libef.h
@@ -26,7 +26,7 @@
 #endif
 extern "C" FRAMEWORK_EXPORT bool QueryInterface(
     /*[in]*/ const char* interface_name,
-    /*[out]*/ void** interface);
+    /*[out]*/ void** iface);
 
 class IBaseRef {
  public:
@@ -38,23 +38,27 @@ class IBaseRef {
 class IBaseInterface : public IBaseRef {
  public:
   /**
-   * \brief 通过接口名称获取指定接口实例对象
+   * @brief Query interface with name
    *
-   * \param interface_name 接口名称
-   * \param interface 接口实例对象
-   * \return true 获取成功,false 获取失败
+   * @param interface_name 
+   * @param interface 
+   * @return true on success,false on failure.
    */
-  virtual bool QueryInterface(const char* interface_name, void** interface) = 0;
+  virtual bool QueryInterface(const char* interface_name, void** iface) = 0;
   /**
-   * \brief 获取对应接口版本号.
+   * @brief Get interface version
    *
-   * \return 接口版本号
+   * @return version
    */
   virtual unsigned int GetVersion() const = 0;
 };
 
 class ITask : public IBaseRef {
  public:
+ /**
+  * @brief Execute task
+  * 
+  */
   virtual void Run() = 0;
 };
 
@@ -82,9 +86,30 @@ enum class MessageLoopType {
 
 class IMessageLoop : public IBaseInterface {
  public:
+ /**
+  * @brief starting a message loop
+  * 
+  * @return 
+  */
   virtual int Run() = 0;
+
+  /**
+   * @brief quit message loop
+   * 
+   */
   virtual void Quit() = 0;
+  /**
+   * @brief Get message loop type
+   * 
+   * @return UI, IO or default
+   */
   virtual MessageLoopType GetType() = 0;
+
+  /**
+   * @brief 
+   * 
+   * @return true on can post nestable task
+   */
   virtual bool IsNestable() const = 0;
 };
 
@@ -96,18 +121,45 @@ using ThreadId = pid_t;
 
 class IThread : public IBaseRef {
  public:
+ /**
+  * @brief Start thread
+  * 
+  */
   virtual void Start() = 0;
+
+  /**
+   * @brief Stop thread
+   * 
+   */
   virtual void Stop() = 0;
+
+  /**
+   * @brief 
+   * 
+   * @return true if thread is running, otherwise false
+   */
   virtual bool IsRunning() const = 0;
+
+  /**
+   * @brief Get thread name
+   * 
+   */
   virtual const char* GetName() const = 0;
   /**
-   * \brief Get thread id.
+   * @brief Get thread id.
    * Note: If the thread is not started,
    * it will wait until the thread is run and then return the ID of this thread
    *
-   * \return thread id
+   * @return thread id
    */
   virtual ThreadId GetThreadId() const = 0;
+
+  /**
+   * @brief Get current thread's task runner
+   * 
+   * @param runner 
+   * @return true on success, otherwise failure
+   */
   virtual bool GetTaskRunner(ITaskRunner** runner) const = 0;
 };
 
@@ -121,16 +173,47 @@ class IFramework : public IBaseInterface {
    * @param instance Process instance
    * @param create_thread_pool support thread pool. if true, than call
    *            CreateThreadPoolTaskRunner will be success, otherwise failed.
-   * @return true on success, false on failed.
+   * @return true on success, false on failure.
    */
   virtual bool Initialize(void* instance, bool create_thread_pool) = 0;
+
+  /**
+   * @brief
+   *
+   */
   virtual void UnInitialize() = 0;
+
+  /**
+   * @brief Create a Message Loop object
+   *
+   * @param nestable
+   * @param type
+   * @param loop
+   * @return true on success, otherwise failure
+   */
   virtual bool CreateMessageLoop(bool nestable,
                                  MessageLoopType type,
                                  IMessageLoop** loop) = 0;
+
+  /**
+   * @brief Create a Thread object
+   *
+   * @param type
+   * @param name
+   * @param thread
+   * @return true on success, false on failure
+   */
   virtual bool CreateThread(MessageLoopType type,
                             const char* name,
                             IThread** thread) = 0;
+
+  /**
+   * @brief Create a Thread Pool Task Runner object
+   *
+   * @param mode
+   * @param task_runner
+   * @return true on success, false on failure
+   */
   virtual bool CreateThreadPoolTaskRunner(ThreadPoolTaskRunnerMode mode,
                                           ITaskRunner** task_runner) = 0;
 };
